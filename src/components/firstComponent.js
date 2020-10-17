652a02aab7eaa6b16f22a41cf4bc89f7497866fa
Anshul-Garg-15 <singhvineet4@gmail.com>
2020-10-17 11:30:56 +0530

    Set up directory structure

---
+++ src/components/firstComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class firstComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
